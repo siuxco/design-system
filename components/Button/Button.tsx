d81d6a882052a3e5674d165dd6948750306850a7
Marcos Sanchez <sanchez.marcos.germ@gmail.com>
2023-03-30 21:18:57 -0300

    refactor(buttons) Removed background-opacity

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -7,36 +7,37 @@ const variants = {
     // Primary buttons
     primary: 'background-primary-7 hover:background-primary-5 active:background-primary-8 color-white',
     'primary-light':
-      'background-primary-1 color-primary-7 hover:background-opacity-primary-4 active:background-opacity-primary-5',
-    'primary-link': 'background-transparent color-primary-7 hover:background-primary-1 active:background-primary-2',
+      'background-primary-1 color-primary-7 hover:color-primary-8 hover:background-primary-2 active:background-primary-3',
+    'primary-link': 'color-primary-7 hover:color-primary-5 active:color-primary-7',
 
     // Secondary buttons
     secondary: 'background-secondary-7 hover:background-secondary-6 active:background-secondary-8 color-white',
     'secondary-light':
-      'background-secondary-1 color-secondary-7 hover:color-secondary-8 hover:background-opacity-secondary-4 active:background-opacity-secondary-5',
-    'secondary-link':
-      'background-transparent color-secondary-7 hover:background-secondary-1 active:background-secondary-2',
+      'background-secondary-1 color-secondary-7 hover:color-secondary-8 hover:background-secondary-2 active:background-secondary-3',
+    'secondary-link': 'color-secondary-7 hover:color-secondary-5 active:color-secondary-7',
 
     // Tertiary buttons
     tertiary: 'background-tertiary-7 hover:background-tertiary-6 active:background-tertiary-8 color-white',
     'tertiary-light':
-      'background-tertiary-1 color-tertiary-7 hover:color-tertiary-8 hover:background-opacity-tertiary-4 active:background-opacity-tertiary-5',
-    'tertiary-link': 'background-transparent color-tertiary-7 hover:background-tertiary-1 active:background-tertiary-2',
+      'background-tertiary-1 color-tertiary-7 hover:color-tertiary-8 hover:background-tertiary-2 active:background-tertiary-3',
+    'tertiary-link': 'color-tertiary-7 hover:color-tertiary-5 active:background-tertiary-7',
 
     // Cancel buttons
     cancel: 'background-neutral-7 hover:background-neutral-6 color-white active:background-neutral-8',
     'cancel-light':
-      'background-neutral-1 color-neutral-7 hover:color-neutral-8 hover:background-opacity-neutral-4 active:background-opacity-neutral-6',
-    'cancel-link': 'background-transparent color-neutral-7 hover:background-neutral-1 active:background-neutral-2',
+      'background-neutral-1 color-neutral-7 hover:color-neutral-8 hover:background-neutral-2 active:background-neutral-3',
+    'cancel-link': 'color-neutral-7 hover:color-neutral-5 active:color-neutral-7',
 
     // Delete buttons
     delete: 'background-error-7 hover:background-error-6 color-white',
     'delete-light':
-      'background-error-1 color-error-7 hover:color-error-8 hover:background-opacity-error-4 active:background-opacity-error-6',
+      'background-error-1 color-error-7 hover:color-error-8 hover:background-error-2 active:background-error-3',
 
     // Black and white
     black: 'background-black hover:background-opacity-black-7 color-white active:background-opacity-black-8',
+    'black-link': 'color-black hover:color-neutral-7 active:color-black',
     white: 'background-white hover:background-opacity-neutral-2 color-black active:background-neutral-2',
+    'white-link': 'color-white hover:color-neutral-2 active:color-white',
 
     // Disabled
     disabled: 'background-neutral-1 color-neutral-5 cursor-not-allowed',
@@ -82,7 +83,9 @@ export interface IButtonProperties extends ComponentProps<'button'> {
     | 'delete'
     | 'delete-light'
     | 'black'
-    | 'white';
+    | 'black-link'
+    | 'white'
+    | 'white-link';
 }
 
 const Button = forwardRef<HTMLButtonElement, IButtonProperties>(
